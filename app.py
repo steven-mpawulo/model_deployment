76f00d986c52cb825c05328ec82a7612304deda4
Kasuletrevor <kasulejohntrevor@gmail.com>
2022-10-21 17:08:14 +0300

    added index.html and upload route

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-
-import random
-import os
-from flask import Flask, request, jsonify
-
 from keyword_spotting_service import _Keyword_Spotting_Service, Keyword_Spotting_Service
+import random
+from flask import Flask, render_template,jsonify,request
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+import os
+from wtforms.validators import InputRequired
 
-
-# instantiate flask app
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['UPLOAD_FOLDER'] = 'static/files'
+
+class UploadFileForm(FlaskForm):
+    file = FileField("File", validators=[InputRequired()])
+    submit = SubmitField("Upload File")
+
+@app.route('/', methods=['GET',"POST"])
+def home():
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data # First grab the file
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
+        return "File has been uploaded."
+    return render_template('index.html', form=form)
+
+@app.route('/home', methods=['GET',"POST"])
 
 
 @app.route("/predict", methods=["POST"])
@@ -46,4 +63,4 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
