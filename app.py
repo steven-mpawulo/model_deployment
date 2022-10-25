e8d2ccfc72e36b56e5ed0a42fb5d775ac21f4f1e
steven-mpawulo <stevenmpawulo@gmail.com>
2022-10-25 14:01:10 +0300

    works on upload and predict

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+from unittest import result
 from keyword_spotting_service import _Keyword_Spotting_Service, Keyword_Spotting_Service
 import random
-from flask import Flask, render_template,jsonify,request
+from flask import Flask, render_template, jsonify, request
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
@@ -11,22 +12,28 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['UPLOAD_FOLDER'] = 'static/files'
 
+
 class UploadFileForm(FlaskForm):
     file = FileField("File", validators=[InputRequired()])
     submit = SubmitField("Upload File")
 
-@app.route('/', methods=['GET',"POST"])
+
+@app.route('/', methods=['GET', "POST"])
 def home():
     form = UploadFileForm()
     if form.validate_on_submit():
-        file = form.file.data # First grab the file
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
-        return "File has been uploaded."
-    return render_template('index.html', form=form)
+        file = form.file.data  # First grab the file
+        file_name = str(random.randint(0, 100000))
+        file.save(file_name)
+        # file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
+        kss = _Keyword_Spotting_Service()
+        predicted_keyword = kss.predict(str(file_name))
+        os.remove(file_name)
+        return f'The agricultural keyword is : {predicted_keyword}'
+    return render_template('index.html', form=form,)
 
-@app.route('/home', methods=['GET',"POST"])
 
-
+@app.route('/home', methods=['GET', "POST"])
 @app.route("/predict", methods=["POST"])
 #  def predict():
 # #      """Endpoint to predict keyword
